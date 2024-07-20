475ab96eb2f1b0fc2439a00e96cd18693e10081e
imanbakhtiari <imanbakhtiarinet@basa.ir>
2024-07-20 20:19:46 +0330

    staff-attendance

--- database_setup.py
+++ database_setup.py
@@ -1,5 +1,6 @@
 import os
 import psycopg2
+from psycopg2 import sql
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -11,7 +12,30 @@ DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 
-def create_db():
+def create_database():
+    try:
+        # Connect to the default database
+        conn = psycopg2.connect(dbname='postgres', user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
+        conn.autocommit = True
+        c = conn.cursor()
+
+        # Check if the database exists
+        c.execute(sql.SQL("SELECT 1 FROM pg_database WHERE datname = %s"), [DB_NAME])
+        exists = c.fetchone()
+
+        # Create the database if it doesn't exist
+        if not exists:
+            c.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier(DB_NAME)))
+            print(f"Database '{DB_NAME}' created successfully.")
+        else:
+            print(f"Database '{DB_NAME}' already exists.")
+    except psycopg2.Error as e:
+        print(f"Database error: {e}")
+    finally:
+        if conn is not None:
+            conn.close()
+
+def create_db_schema():
     try:
         conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
         conn.autocommit = True
@@ -42,5 +66,6 @@ def create_db():
             conn.close()
 
 if __name__ == "__main__":
-    create_db()
+    create_database()
+    create_db_schema()
 
